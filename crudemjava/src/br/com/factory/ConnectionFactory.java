8cc12ded15e126e2ca07569825e03b5d5cae9b43
Arthur Tavares <112263960+tavaresProg@users.noreply.github.com>
2022-12-27 17:28:01 -0300

    Update ConnectionFactory.java

--- crudemjava/src/br/com/factory/ConnectionFactory.java
+++ crudemjava/src/br/com/factory/ConnectionFactory.java
@@ -6,23 +6,15 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-	// Nome do usuário do MySql
-	private static final String USERNAME = "SEU USERNAME AQUI";
-
-	// Senha do banco
-	private static final String PASSWORD = "SUA SENHA AQUI";
-
-	// Caminho do banco de dados, porta e o nome do banco de dados
-	private static final String DATABASE_URL = "jdbc:mysql://localhost/agenda?useTimezone=true&serverTimezone=UTC(EXEMPLO DE CAMINHO PARA O BANCO)";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "root";
+	private static final String DATABASE_URL = "jdbc:mysql://localhost/agenda?useTimezone=true&serverTimezone=UTC";
 
 	/*
 	 * Conexão com o banco de dados
 	 */
 	public static Connection creatConnectionToMySQL() throws SQLException, ClassNotFoundException {
-		// Faz com que a classe seja carregada pela JVM
-		//Class.forName("com.mysql.jdbc.Driver");
 
-		// Cria a conexão com o banco de dados
 		Connection connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
 
 		return connection;
@@ -30,10 +22,8 @@ public class ConnectionFactory {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-		// Recuperar uma coneão com o banco de dados
 		Connection con = creatConnectionToMySQL();
 
-		// Testar se há conexão
 		if (con != null) {
 			System.out.println("Conexao obtida com sucesso!");
 			con.close();
